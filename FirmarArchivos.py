720daa802c63879fbfaa47fcf9fbd92e00a3b23d
RomanCray <santipau-1998@hotmail.com>
2023-11-14 13:55:41 -0500

    Version 1.1.7

--- FirmarArchivos.py
+++ FirmarArchivos.py
@@ -1,5 +1,6 @@
 import os
 import math
+import requests
 import datetime
 from endesive.pdf import cms
 from cryptography import x509
@@ -19,83 +20,116 @@ def firmar(pdf,datos,newPDF,company,dni):
     date = datetime.datetime.utcnow() - datetime.timedelta(hours=12)           
 
     try:
-        for data in datos:            
+        print("LEE EL PDF")
+        datau = pdf.read() 
+ 
+        print(datos)
+        i=0
+        for dato in datos:
+            firma = dato['firmaNom']
+            contra = dato["password"]
+            pagina = dato["paginas_a_firmar"]
+            pre_x0 = dato["x0"]
+            pre_y0 = dato["y0"]
+            pre_x1 = dato["x1"]
 
-            # Abre el archivo PDF como un objeto de archivo binario
-            print("LEE EL PDF")
-            # with open(pdf, 'rb') as pdf_file:
-            datau = pdf.read() 
+            nombre_completo = firma
+            nombrePartes = nombre_completo.split()
+            print(nombrePartes[0])
+            print(nombrePartes[1])
+            print(nombrePartes[2])
+            print(nombrePartes[3])            
 
-            datos_obj = json.loads(data)            
-            i=0
-            for dato in datos_obj:
-                firma = dato.get("firma", {})
-                contra = dato.get("password")                
-                pagina = dato.get("paginas_a_firmar")
-                pre_x0 = dato.get("x0")
-                pre_y0 = dato.get("y0")
-                pre_x1 = dato.get("x1")                                 
+            url = 'http://localhost/SIER/rel/ho/firma'
+            dats = {
+                    'nom1': nombrePartes[0],
+                    'nom2': nombrePartes[1],
+                    'apell1':nombrePartes[2],
+                    'apell2':nombrePartes[3]
+                }
+            
+            response = requests.post(url, data=dats)
+            
+            if response.status_code == 200:    
 
-                # Abre el archivo P12 como un objeto de archivo binario Y LEE LA INFORMACION Q HAY DENTRO   
-                print("LEE EL p12")
-                with open(firma['name'], 'rb') as p12_file:    
-                    p12 = pkcs12.load_key_and_certificates(
+                date_str = date.strftime('%Y-%m-%d_%H-%M-%S_%f')
+                nombreArchTemp = os.path.splitext(os.path.basename(pdf.filename))[0]
+
+                # MANDO A GUARDA EN ESA RUTA EL NUEVO ARCHIVO  
+                if not os.path.exists(f'P12_TEMPORAL/{dni}'):
+                    os.makedirs(f'P12_TEMPORAL/{dni}')       
+
+                output_p12_path = f'P12_TEMPORAL/{dni}/TEMPORAL_{date_str}_{nombreArchTemp}.p12'                
+                print(output_p12_path)                
+
+                with open(output_p12_path, 'wb') as f:
+                    f.write(response.content)
+                print('Archivo descargado exitosamente.')
+            else:
+                print(f'Error en la solicitud. Código de respuesta: {response.status_code}')
+
+            # Abre el archivo P12 como un objeto de archivo binario Y LEE LA INFORMACION Q HAY DENTRO   
+            print("LEE EL p12")
+            with open(output_p12_path, 'rb') as p12_file:    
+                p12 = pkcs12.load_key_and_certificates(
                         p12_file.read(), contra.encode("ascii"), backends.default_backend()
-                )      
+                    )      
 
-                # Obtener el campo CN (Nombre titular)
-                cert = p12[1]
-                P12_Nobre_remitente = cert.subject.get_attributes_for_oid(x509.NameOID.COMMON_NAME)[0].value                  
+            # Obtener el campo CN (Nombre titular)
+            cert = p12[1]
+            P12_Nobre_remitente = cert.subject.get_attributes_for_oid(x509.NameOID.COMMON_NAME)[0].value
+            print(P12_Nobre_remitente)   
 
-                # ESTABLESO LOS PARAMETROS QUE VA TENER LA FIRMA
-                date1 = date.strftime("D:%Y%m%d%H%M%S+00'00'")        
-                nombreImgTemp = generadorQR(P12_Nobre_remitente,date1)                                
+            # ESTABLESO LOS PARAMETROS QUE VA TENER LA FIRMA
+            date1 = date.strftime("D:%Y%m%d%H%M%S+00'00'")        
+            # nombreImgTemp = generadorQR(P12_Nobre_remitente,date1)              
 
-                nombre_archivo = os.path.basename(pdf.filename)
+            nombre_archivo = os.path.basename(pdf.filename)
 
-                print("POSICIONES")
-                x0 = pre_x0 - 40
-                y0 = pre_y0 -220
-                x1= pre_x1 + 40
-                y1= y0 +100
-                print(i,x0,y0,x1,y1,pagina)
+            print("POSICIONES")
+            x0 = pre_x0 - 40
+            y0 = pre_y0 -220
+            x1= pre_x1 + 40
+            y1= y0 +100
+            print(i,x0,y0,x1,y1,pagina)
 
 
-                #  794 x 1123 píxeles
-                dct.append({
-                        "aligned": 0,
-                        "sigflags": 3,
-                        "sigflagsft": 132,
-                        "sigpage": int(pagina),
-                        "sigbutton": True,
-                        "sigfield": f"Signature{i}",
-                        "auto_sigfield": True,
-                        "sigandcertify": True,
-                        # "signaturebox": (0,600,200,700),
-                        "signaturebox": (x0,y0,x1,y1),
-                        "signature_img": f"img/{nombreImgTemp}",
-                        "signature": "",        
-                        "contact": "hola@ejemplo.com",
-                        "location": "QUITO",
-                        "signingdate": date1,
-                        "reason": "PRUEBAS INTHELO",
-                        "password": contra
-                    })
-                print(dct[-1])
+            #  794 x 1123 píxeles
+            dct.append({
+                    "aligned": 0,
+                    "sigflags": 3,
+                    "sigflagsft": 132,
+                    "sigpage": int(pagina),
+                    "sigbutton": True,
+                    "sigfield": f"Signature{i}",
+                    "auto_sigfield": True,
+                    "sigandcertify": True,                    
+                    "signaturebox": (x0,y0,x1,y1),
+                    # "signature_img": f"img/{nombreImgTemp}",
+                    "signature": "",        
+                    "contact": "hola@ejemplo.com",
+                    "location": "QUITO",
+                    "signingdate": date1,
+                    "reason": "PRUEBAS INTHELO",
+                    "password": contra
+                })
+            
+            print(dct[-1])
 
-                # CON ESTO USO EL ARCHIVO Q SE ESTA LEYENDO Y AGREGO LA FIRMA
-                print("FIRMA EL PDF CON EL p12")
-                datas1 = cms.sign(datau, dct[-1], p12[0], p12[1], p12[2], "sha256")
-                datas.append(datas1)
+            # CON ESTO USO EL ARCHIVO Q SE ESTA LEYENDO Y AGREGO LA FIRMA
+            print("FIRMA EL PDF CON EL p12")
+            datas1 = cms.sign(datau, dct[-1], p12[0], p12[1], p12[2], "sha256")
+            datas.append(datas1)
 
-                # BUELVO A UNIR TODAS LAR PARTES EL PDF Y LA FIRMA CON TODOS LOS CRETIFICADOS
-                archivo_pdf_para_enviar_al_cliente.write(datau) 
-                archivo_pdf_para_enviar_al_cliente.write(datas[-1])
-                archivo_pdf_para_enviar_al_cliente.seek(0)
+            # BUELVO A UNIR TODAS LAR PARTES EL PDF Y LA FIRMA CON TODOS LOS CRETIFICADOS
+            archivo_pdf_para_enviar_al_cliente.write(datau) 
+            archivo_pdf_para_enviar_al_cliente.write(datas[-1])
+            archivo_pdf_para_enviar_al_cliente.seek(0)
 
-                i += 1
+            i += 1
+            os.remove(output_p12_path)            
 
-            print("CREA EL NUEVO ARCHIVO")
+        print("CREA EL NUEVO ARCHIVO")
 
         date_str = date.strftime('%Y-%m-%d_%H-%M-%S_%f') 
 
